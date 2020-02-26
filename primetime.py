83665ca2572a2bbf35764a105f368f7eb61d69d7
EC2 Default User <ec2-user@ip-172-31-84-47.ec2.internal>
2020-02-26 21:45:39 +0000

    primetime

--- primetime.py
+++ primetime.py
@@ -1,19 +1,20 @@
 from twilio.rest import Client
 import time 
 
-account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-auth_token = 'your_auth_token'
+account_sid = 'AC239248855693095338a44f03f7a37803'
+auth_token = 'a867e61f710d60e5e0e2f6e7bd8097f7'
 client = Client(account_sid, auth_token)
 
-
-while(true):
-    time.sleep(5000)
+x = 0
+while(x<3):
+    time.sleep(5)
     print("tik")
     message = client.messages \
                 .create(
                      body="Poke.",
-                     from_='+15017122661',
-                     to='+15558675310'
-                 )
+                     from_='+12015716393',
+                     to='+12674756252' 
+                )
 
     print(message.sid)
+    x += 1
