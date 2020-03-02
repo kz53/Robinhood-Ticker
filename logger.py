1d0e28c0798b3e466e13ec2f069d1d1d45c11787
EC2 Default User <ec2-user@ip-172-31-84-47.ec2.internal>
2020-03-02 15:07:27 +0000

    stash

--- logger.py
+++ logger.py
@@ -1,16 +1,20 @@
 from time import sleep
 from datetime import datetime
+import pytz
+import sys
 
 def time_stamp():
     utc_now = pytz.utc.localize(datetime.utcnow())
     est_now = utc_now.astimezone(pytz.timezone("America/New_York")) 
-    dt_string = est_0now.strftime("%b-%d, %Y: %H:%M:%S")
+    dt_string = est_now.strftime("%b-%d: %H:%M:%S \n")
     print(dt_string)
     return dt_string
 
 f = open("test-log-file.txt", "a")
-f.write(time_stamp())
-while(true):
+f.write("Time log:\n")
+while(True):
+    #print("hello world")
     f.write(time_stamp())
-    sleep(5)
+    f.flush()
+    sleep(2)
 f.close()
