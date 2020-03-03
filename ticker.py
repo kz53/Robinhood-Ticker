149f0eff68ecfa4af79c3e5cdb3217833bb34b21
EC2 Default User <ec2-user@ip-172-31-84-47.ec2.internal>
2020-03-03 21:09:50 +0000

    first batch of data mining

--- ticker.py
+++ ticker.py
@@ -1,16 +1,29 @@
 from time import sleep
 from datetime import datetime
+import robin_stocks as robin
+import pytz
+import config 
+import pprint
+
+robin.login(config.rh_usr_name, config.rh_password)
 
 def time_stamp():
     utc_now = pytz.utc.localize(datetime.utcnow())
     est_now = utc_now.astimezone(pytz.timezone("America/New_York")) 
-    dt_string = est_0now.strftime("%b-%d, %Y: %H:%M:%S")
+    dt_string = est_now.strftime("%b-%d, %Y: %H:%M:%S") + "\n"
     print(dt_string)
     return dt_string
 
-f = open("test-log-file.txt", "a")
-f.write(time_stamp())
-while(true):
+f = open("raw-output.txt", "a")
+i = 0
+while(i<14400):
     f.write(time_stamp())
-    sleep(5)
+    price = robin.get_latest_price("MSFT")[0]
+    f.write(price+"\n")
+    f.write("--------------------------------\n")
+    f.flush()
+    i += 1
+    sleep(1)
 f.close()
+robin.logout()
+print("finished")
