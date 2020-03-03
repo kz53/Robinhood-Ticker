f09a5d1e31cdb21e45384dc1760032510f1fa795
Kelley Zhang <kelley.zhang@skf.com>
2020-03-03 16:40:52 -0500

    extractor2

--- extractor.py
+++ extractor.py
@@ -1,4 +1,11 @@
 f = open("raw-output.txt", "r")
-n = open("raw-output-numbers.txt", "r")
+n = open("raw-output-numbers.txt", "a")
 
-for s in readlines 
+i = 0
+for s in f.readlines:
+    if i % 3 = 1:
+        n.write(s+"/n")
+    i += 1
+
+f.close()
+n.close()
