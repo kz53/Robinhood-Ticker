bf024417029b61b8182321848831f0611fa8f6a7
Kelley Zhang <kelley.zhang@skf.com>
2020-04-24 02:40:18 -0400

    automatic date in file name

--- robin_helpers.py
+++ robin_helpers.py
@@ -1,3 +1,8 @@
+# data structures
+price_arr = []
+time_price = [(0,165.0)]
+    #(time, objects, volume)
+
 # Returns string in form of "Month Day, Year: hh:mm:ss "
 def time_stamp():
     utc_now = pytz.utc.localize(datetime.utcnow())
@@ -29,23 +34,47 @@ def round_to_2(x , places=2):
     return round(x, places)
 
 def print_account_balance():
-
+    return True
+    
 def print_account_holdings():
-
+    return True
+    
 def print_account_total_value():
-
+    return True
+    
 def print_account_balance():
-
+    return True
+    
 def kill_all_process():
-
+    return True    
 
 def print_order_buy_response():
-
+    return True
+    
 def print_order_sell_value():
-
+    return True
+    
 def print_portfolio():
     for s in portfolio:
         print(s)
 
-
 def output_to_log_file():
+    return True
+
+def SMA(time_frame, start_time=None):
+    if start_time is None: 
+        data_points = price_tuple_arr[(start_time-1*time_frame):start_time] 
+    else: 
+        data_points = price_tuple_arr[:(-1*time_frame)]
+    return sum(data_points)/time_frame
+
+def slo(start, time_interval): 
+    t2 = start
+    t1 = start - time_interval
+    return (time_price[t2][1]-time_price[t1][1])/time_interval
+
+def linreg(time_frame, start_time=None):
+    get_valid_pts()
+
+
+    return slope
