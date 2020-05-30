86b85ca8af3fcf5e402cd3d0ac186dc5dd03740e
Kelley Zhang <kz53@cornell.edu>
2020-05-30 12:24:49 -0400

    save raw output outside repo

--- ticker.py
+++ ticker.py
@@ -22,7 +22,7 @@ def time_stamp():
     return dt_string
 
 
-f = open("raw-output-{month_str}-{day_str}-{year_str}.txt", "a")
+f = open("../raw-output-files/{month_str}-{day_str}-{year_str}-raw-output.txt", "a")
 i = 0
 while(i<23500):
     try:
